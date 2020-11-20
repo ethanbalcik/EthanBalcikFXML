fe3968b664374b8694cfad4adbcb4f9ffc3e7ed7
ethanbalcik <emb71@psu.edu>
2020-11-20 15:59:55 -0500

    Additions for question 3

--- src/Controller/FXMLDocumentController.java
+++ src/Controller/FXMLDocumentController.java
@@ -11,10 +11,16 @@ import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Scanner;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
@@ -26,6 +32,22 @@ import javax.persistence.Query;
 public class FXMLDocumentController implements Initializable
 {
     private EntityManager entityManager;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TableView<Usermodel> userTable;
+    @FXML
+    private TableColumn<Usermodel, Integer> idColumn;
+    @FXML
+    private TableColumn<Usermodel, String> usernameColumn;
+    @FXML
+    private TableColumn<Usermodel, String> firstnameColumn;
+    @FXML
+    private TableColumn<Usermodel, String> lastnameColumn;
+    @FXML
+    private TableColumn<Usermodel, String> emailColumn;
+    @FXML
+    private TableColumn<Usermodel, Date> dateOfBirthColumn;
 
     public EntityManager getEntityManager()
     {
@@ -36,17 +58,125 @@ public class FXMLDocumentController implements Initializable
     {
         this.entityManager = entityManager;
     }
+
+    public TextField getUsernameField()
+    {
+        return usernameField;
+    }
+
+    public void setUsernameField(TextField usernameField)
+    {
+        this.usernameField = usernameField;
+    }
+
+    public TableView<Usermodel> getUserTable()
+    {
+        return userTable;
+    }
+
+    public void setUserTable(TableView<Usermodel> userTable)
+    {
+        this.userTable = userTable;
+    }
+
+    public TableColumn<Usermodel, Integer> getIdColumn()
+    {
+        return idColumn;
+    }
+
+    public void setIdColumn(TableColumn<Usermodel, Integer> idColumn)
+    {
+        this.idColumn = idColumn;
+    }
+
+    public TableColumn<Usermodel, String> getUsernameColumn()
+    {
+        return usernameColumn;
+    }
+
+    public void setUsernameColumn(TableColumn<Usermodel, String> usernameColumn)
+    {
+        this.usernameColumn = usernameColumn;
+    }
+
+    public TableColumn<Usermodel, String> getFirstnameColumn()
+    {
+        return firstnameColumn;
+    }
+
+    public void setFirstnameColumn(TableColumn<Usermodel, String> firstnameColumn)
+    {
+        this.firstnameColumn = firstnameColumn;
+    }
+
+    public TableColumn<Usermodel, String> getLastnameColumn()
+    {
+        return lastnameColumn;
+    }
+
+    public void setLastnameColumn(TableColumn<Usermodel, String> lastnameColumn)
+    {
+        this.lastnameColumn = lastnameColumn;
+    }
+
+    public TableColumn<Usermodel, String> getEmailColumn()
+    {
+        return emailColumn;
+    }
+
+    public void setEmailColumn(TableColumn<Usermodel, String> emailColumn)
+    {
+        this.emailColumn = emailColumn;
+    }
+
+    public TableColumn<Usermodel, Date> getDateOfBirthColumn()
+    {
+        return dateOfBirthColumn;
+    }
+
+    public void setDateOfBirthColumn(TableColumn<Usermodel, Date> dateOfBirthColumn)
+    {
+        this.dateOfBirthColumn = dateOfBirthColumn;
+    }
     
     @FXML
     public void searchUsers()
     {
-        System.out.println("Clicked!");
+        //Read text in search input field
+        String searchInput = getUsernameField().getText();
+        
+        //Create query based on input
+        Query query = getEntityManager().createNamedQuery("Usermodel.findByUsername");
+        query.setParameter("username", searchInput);
+        
+        //Save the query's result list to a Usermodel list
+        List<Usermodel> users = query.getResultList();
+        
+        //Initialize ObservableList and add users to it
+        ObservableList<Usermodel> userData = FXCollections.observableArrayList();
+        for(Usermodel u : users)
+        {
+            userData.add(u);
+        }
+        
+        //Set ObservableList as the TableView items list
+        getUserTable().setItems(userData);
+        getUserTable().refresh();
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
+        //Initialize EntityManager
         setEntityManager((EntityManager) Persistence.createEntityManagerFactory("EthanBalcikFXMLPU").createEntityManager());
+        
+        //Initialize TableColumns
+        getIdColumn().setCellValueFactory(new PropertyValueFactory<>("userid"));
+        getUsernameColumn().setCellValueFactory(new PropertyValueFactory<>("username"));
+        getFirstnameColumn().setCellValueFactory(new PropertyValueFactory<>("firstname"));
+        getLastnameColumn().setCellValueFactory(new PropertyValueFactory<>("lastname"));
+        getEmailColumn().setCellValueFactory(new PropertyValueFactory<>("email"));
+        getDateOfBirthColumn().setCellValueFactory(new PropertyValueFactory<>("dateofbirth"));
     }    
     
 }
